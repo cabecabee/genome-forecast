2c476a4afa8695851a04672d9ae6cce2fa7be30d
cabecabe <bernardomb004@gmail.com>
2025-08-23 16:29:42 +0000

    Sistema para a leitura de arquivo FASTA introduzido

--- tests/testing.py
+++ tests/testing.py
@@ -2,5 +2,5 @@ from Bio import SeqIO
 
 for record in SeqIO.parse("exemplo.fasta", "fasta"):
     print("Nome:", record.id)       # nome da sequência
-    print("Descrição:", record.description)  # header completo (opcional)
+    print("Descrição:", record.description)
     print("Sequência:", str(record.seq))
