e62cbab53459335577bedd366f266c6d607a90cb
cabecabe <bernardomb004@gmail.com>
2025-08-27 10:37:16 -0300

    Delete tests directory

--- tests/testing.py
+++
@@ -1,6 +0,0 @@
-from Bio import SeqIO
-
-for record in SeqIO.parse("exemplo.fasta", "fasta"):
-    print("Nome:", record.id)       # nome da sequência
-    print("Descrição:", record.description)
-    print("Sequência:", str(record.seq))
