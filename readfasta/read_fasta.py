5960051a26b75a3006faacb5c5a5c6a77c616429
cabecabe <bernardomb004@gmail.com>
2025-08-27 20:12:40 +0000

    A função read_fasta agora consegue identificar o ID, Descrição e Sequência do arquivo FASTA

--- readfasta/read_fasta.py
+++ readfasta/read_fasta.py
@@ -1,9 +1,30 @@
 def read_fasta(filepath):
     with open(filepath, 'r') as file:
-        name = None
-        desc = None
+        header = None
         seq = []
         for line in file:
-            print(line)
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith(">"):
+                if header is not None:
+                    yield{
+                        "id": header.split()[0],
+                        "desc": header,
+                        "seq": "".join(seq)
+                    }
+                header = line[1:]
+                seq = []
+            else:
+                seq.append(line)
+        if header is not None:
+            yield {
+                "id": header.split()[0],
+                "desc": header,
+                "seq": "".join(seq)
+            }
 
-read_fasta("fastafiles/homo.fasta")
+for i in read_fasta("fastafiles/homo.fasta"):
+    print("ID:", i["id"])
+    print("Descrição:", i["desc"])
+    print("Sequência:", i["seq"])
