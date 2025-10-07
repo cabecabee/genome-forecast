04c6714efdaaf67d71bf51b144a0aa385014380f
cabecabe <bernardomb004@gmail.com>
2025-10-07 12:34:13 +0000

    Função translation com o código pronto, apenas faltando ser integrada em uma declaração def

--- functions/translation.py
+++ functions/translation.py
@@ -13,7 +13,7 @@ translate_table = {
     "Ile": {"AUU", "AUC", "AUA"},
     "Met": {"AUG"},
     "Thr": {"ACU", "ACC", "ACG", "ACA"},
-    "Asn": {"AAU, AAC"},
+    "Asn": {"AAU", "AAC"},
     "Lys": {"AAA", "AAG"},
     "Val": {"GUU", "GUC", "GUA", "GUG"},
     "Ala": {"GCU", "GCC", "GCA", "GCG"},
@@ -21,3 +21,20 @@ translate_table = {
     "Glu": {"GAA", "GAG"},
     "Gly": {"GGU", "GGC", "GGA", "GGG"}
 }
+
+# isto ainda não é uma função!!!! quando estiver em casa eu transformo em uma :)
+
+rna = "CGCAAUGGCGGA"
+codons = []
+if len(rna) % 3 != 0:
+    print("Não é múltiplo de 3.")
+else:
+    codons = [rna[i:i+3] for i in range(0, len(rna), 3)]
+
+aminoacidos = []
+for codon in codons:
+    for amino, codonset in translate_table.items():
+        if codon in codonset:
+            aminoacidos.append(amino)
+            break
+print(aminoacidos)
