cfd8bafa2ea8c6d53a27cc298bbd8df9712476b3
cabecabe <bernardomb004@gmail.com>
2025-10-08 14:23:47 +0000

    arquivo de debug e tp53 completa

---
+++ debug.py
@@ -0,0 +1,10 @@
+from functions.read_fasta import read_fasta
+from functions.transcript import transcript
+from functions.translate import translate
+
+dna = ""
+for i in read_fasta("fastafiles/tp53.fasta"):
+    dna = dna.join(i["seq"])
+rna = transcript(dna)
+amino = translate(rna)
+print(amino)
