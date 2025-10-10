e00ead57b6666de3df64041e196cb36eca608cd4
cabecabe <bernardomb004@gmail.com>
2025-10-10 11:31:37 +0000

    A leitura do gene agora lê cada proteína, não só a primeira.

--- functions/transcript.py
+++ functions/transcript.py
@@ -1,19 +1,2 @@
 def transcript(dna):
-
-    #transcricao = {
-    #        'A': 'A',
-    #        'T': 'U',
-    #        'C': 'C',
-    #        'G': 'G'
-    #    }
-
-    #rna = ''.join([transcricao[base] for base in dna])
-    #return rna
-
-    return dna.replace("T", "U")
-
-#dna = "ATCGATCGATCG"
-#print(transcript(dna))
-
-
-# dna.replace("T", "U")
+    return dna.replace("T", "U") # muito obrigado sor frederico
--- functions/translate.py
+++ functions/translate.py
@@ -1,45 +1,46 @@
-from functions.transcript import transcript
-from functions.read_fasta import read_fasta
 def translate(rna):
-    translate_table = {
-        "Phe": {"UUU", "UUC"},
-        "Leu": {"UUA", "UUG", "CUU", "CUC", "CUA", "CUG", "AGU", "AGC"},
-        "Ser": {"UCU", "UCC", "UCA", "UCG"},
-        "Tyr": {"UAU", "UAC"},
-        "stop": {"UAA", "UAG", "UGA"},
-        "Cys": {"UGU", "UGC"},
-        "Trp": {"UGG"},
-        "Pro": {"CCU", "CCC", "CCA", "CCG"},
-        "His": {"CAU", "CAC"},
-        "Gln": {"CAA", "CAG"},
-        "Arg": {"CGU", "CGC", "CGA", "CGG", "AGA", "AGG"},
-        "Ile": {"AUU", "AUC", "AUA"},
-        "Met": {"AUG"},
-        "Thr": {"ACU", "ACC", "ACG", "ACA"},
-        "Asn": {"AAU", "AAC"},
-        "Lys": {"AAA", "AAG"},
-        "Val": {"GUU", "GUC", "GUA", "GUG"},
-        "Ala": {"GCU", "GCC", "GCA", "GCG"},
-        "Asp": {"GAU", "GAC"},
-        "Glu": {"GAA", "GAG"},
-        "Gly": {"GGU", "GGC", "GGA", "GGG"}
+    codon_table = {
+        "UUU": "Phe", "UUC": "Phe",
+        "UUA": "Leu", "UUG": "Leu", "CUU": "Leu", "CUC": "Leu", "CUA": "Leu", "CUG": "Leu",
+        "UCU": "Ser", "UCC": "Ser", "UCA": "Ser", "UCG": "Ser", "AGU": "Ser", "AGC": "Ser",
+        "UAU": "Tyr", "UAC": "Tyr",
+        "UAA": "stop", "UAG": "stop", "UGA": "stop",
+        "UGU": "Cys", "UGC": "Cys",
+        "UGG": "Trp",
+        "CCU": "Pro", "CCC": "Pro", "CCA": "Pro", "CCG": "Pro",
+        "CAU": "His", "CAC": "His",
+        "CAA": "Gln", "CAG": "Gln",
+        "CGU": "Arg", "CGC": "Arg", "CGA": "Arg", "CGG": "Arg", "AGA": "Arg", "AGG": "Arg",
+        "AUU": "Ile", "AUC": "Ile", "AUA": "Ile",
+        "AUG": "Met",
+        "ACU": "Thr", "ACC": "Thr", "ACA": "Thr", "ACG": "Thr",
+        "AAU": "Asn", "AAC": "Asn",
+        "AAA": "Lys", "AAG": "Lys",
+        "GUU": "Val", "GUC": "Val", "GUA": "Val", "GUG": "Val",
+        "GCU": "Ala", "GCC": "Ala", "GCA": "Ala", "GCG": "Ala",
+        "GAU": "Asp", "GAC": "Asp",
+        "GAA": "Glu", "GAG": "Glu",
+        "GGU": "Gly", "GGC": "Gly", "GGA": "Gly", "GGG": "Gly"
     }
-    metindex = -1
-    for i in range(len(rna) - 2):
+
+    proteins = []
+    i = 0
+    while i < len(rna) - 2:
         codon = rna[i:i+3]
         if codon == "AUG":
-            metindex = i
-            break
-    if metindex == -1:
-        return []
-    
-    aminoacidos = []
-    for i in range(metindex, len(rna)-2, 3):
-        codon = rna[i:i+3]
-        if codon in translate_table["stop"]:
-            break
-        for amino, codonset in translate_table.items():
-            if codon in codonset:
-                aminoacidos.append(amino)
-                break
-    return aminoacidos
+            protein = []
+            j = i
+            while j < len(rna) - 2:
+                current_codon = rna[j:j+3]
+                amino = codon_table.get(current_codon, None)
+                if amino == "stop":
+                    break
+                elif amino:
+                    protein.append(amino)
+                j += 3
+            if protein:
+                proteins.append(protein)
+            i = j + 3
+        else:
+            i += 1
+    return proteins
