1d35107692dfc34b28d82254bac900a0fb674142
cabecabe <bernardomb004@gmail.com>
2025-10-14 12:34:49 +0000

    Removido um import desnecessário do módulo isdna

--- debug.py
+++ debug.py
@@ -1,7 +1,6 @@
 from functions.read_fasta import read_fasta
 from functions.transcript import transcript
 from functions.translate import translate
-from functions.isdna import isdna
 
 dna = ""
 for i in read_fasta("fastafiles/p53.fasta"):
