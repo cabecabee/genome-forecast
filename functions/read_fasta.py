42068889934108b11905129c2b0b296b5925209d
cabecabe <bernardomb004@gmail.com>
2025-10-19 23:54:15 +0000

    __init__.py criado, e read_fasta teve alguns pequenos erros corrigidos

--- functions/read_fasta.py
+++ functions/read_fasta.py
@@ -1,35 +1,39 @@
 from functions.isdna import isdna
+
 def read_fasta(filepath):
-    if filepath.endswith((".fasta", ".fas", ".fa", ".frn", ".fna", ".ffn")) and isdna(filepath):
-        with open(filepath, 'r') as file:
-            header = None
-            seq = []
-            for line in file:
-                line = line.strip()
-                if not line:
-                    continue
-                if line.startswith(">"):
-                    if header is not None:
-                        yield{
-                            "id": header.split()[0],
-                            "desc": header,
-                            "seq": "".join(seq),
-                            "bases": len(seq[0])
-                        }
-                    header = line[1:]
-                    seq = []
-                else:
-                    seq.append(line)
-            if header is not None:
-                yield {
-                    "id": header.split()[0],
-                    "desc": header,
-                    "seq": "".join(seq),
-                    "bases": len(seq[0])
-                }
-    elif not filepath.endswith((".fasta", ".fas", ".fa", ".frn", ".fna", ".ffn")) and not isdna(filepath):
-        print("O arquivo não é uma extensão FASTA de nucleotídeos. (.fasta, .fna, .ffn, .frn, .fa, .fas) e o arquivo não contém apenas ACTG e U.")
-    elif not isdna(filepath):
-        print("O arquivo não contém apenas ACTG e U")
-    elif not filepath.endswith((".fasta", ".fas", ".fa", ".frn", ".fna", ".ffn")):
+    valid_exts = (".fasta", ".fas", ".fa", ".frn", ".fna", ".ffn")
+
+    if not filepath.endswith(valid_exts):
         print("O arquivo não é uma extensão FASTA de nucleotídeos. (.fasta, .fna, .ffn, .frn, .fa, .fas)")
+        return
+
+    if not isdna(filepath):
+        print("O arquivo não contém apenas ACTG e U.")
+        return
+
+    with open(filepath, 'r') as file:
+        header = None
+        seq = []
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith(">"):
+                if header is not None:
+                    yield {
+                        "id": header.split()[0],
+                        "desc": header,
+                        "seq": "".join(seq),
+                        "bases": len("".join(seq))
+                    }
+                header = line[1:]
+                seq = []
+            else:
+                seq.append(line)
+        if header is not None:
+            yield {
+                "id": header.split()[0],
+                "desc": header,
+                "seq": "".join(seq),
+                "bases": len("".join(seq))
+            }
