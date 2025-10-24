444110baa939be007472af479c1b73fbaca3aff9
cabecabe <bernardomb004@gmail.com>
2025-10-24 11:34:19 +0000

    tp53 adicionada, um pouco do código mudado

--- debug.py
+++ debug.py
@@ -1,6 +1,9 @@
 from functions.read_fasta import read_fasta
 from functions.transcript import transcript
 from functions.translate import translate
+from functions.get_cds import get_cds
+from functions.poisson import poisson
+from functions.get_weights import get_weights
 
 # dna = ""
 # for i in read_fasta("fastafiles/p53.fasta"):
@@ -11,12 +14,13 @@ from functions.translate import translate
 
 from functions.mutate import mutate
 from functions.duplicate import duplicate
-duplicate("fastafiles/p53.fasta", "duplicates/")
-mutate(0.5, "fastafiles/p53.fasta")
-
-for i in read_fasta("duplicates/p53.fasta"):
+duplicate("fastafiles/gene.fna", "duplicates/")
+for i in read_fasta("duplicates/gene.fna"):
     dna = "".join(i["seq"])
-rna = transcript(dna)
-amino = translate(rna)
-print(amino)
+cds = get_cds(dna)
+print(len(cds))
+#rna = transcript(dna)
+#amino = translate(rna)
+#print(amino)
 # resultados como esperados!!! vamo gremio
+
--- functions/get_cds.py
+++ functions/get_cds.py
@@ -1,10 +1,20 @@
 def get_cds(seq):
-    start = seq.find("ATG")
-    if start == -1:
-        start = 0
+    cds = ""
+    seq = seq.upper()
     stop_codons = ["TAA", "TAG", "TGA"]
-    for i in range(start, len(seq), 3):
+    i = 0
+    while i < len(seq) - 3:
         codon = seq[i:i+3]
-        if codon in stop_codons:
-            return seq[start:i]
-    return seq[start:]
+        if codon == "ATG":
+            start = i
+            for j in range(start, len(seq), 3):
+                stop_codon = seq[j:j+3]
+                if stop_codon in stop_codons:
+                    cds += seq[start:j]
+                    i = j + 3
+                    break
+            else:
+                cds += seq[start:]
+                break
+        i += 1
+    return cds
