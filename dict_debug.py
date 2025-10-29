f8af2ac6f7840e0ad594dba6607a5bdd939cbb77
cabecabee <bernardomb004@gmail.com>
2025-10-29 17:42:53 -0300

    Debugging continuado, aparentemente o programa funciona perfeitamente.

--- dict_debug.py
+++ dict_debug.py
@@ -1,25 +1,4 @@
-dicionario_sbs4 = {
-    "ACA>A": 0.0421,
-    "ACC>A": 0.0332,
-    "ACG>A": 0.0155,
-    "ACT>A": 0.0294,
-    
-    "ACA>G": 0.0068,
-    "ACC>G": 0.0028,
-    "ACG>G": 0.0012,
-    "ACT>G": 0.0035,
-
-    "ACA>T": 0.0086,
-    "ACC>T": 0.0041,
-    "ACG>T": 0.0007,
-    "ACT>T": 0.0042,
-
-    "ATA>A": 0.0094,
-    "ATC>A": 0.0332,
-    "ATG>A": 0.0155,
-    "ATT>A": 0.0294
-    
-}
+from functions.dict_sbs4 import dicionario_sbs4
 
 hotspots = {
     512: {"G>T": 3.0}
@@ -29,7 +8,7 @@ probabilities_mut = []
 
 from functions.read_fasta import read_fasta
 seq = ""
-for i in read_fasta("duplicates/gene.fna"):
+for i in read_fasta("fastafiles/gene.fna"):
     seq = seq + i["seq"]
 
 for i in range(1, len(seq)-1):
