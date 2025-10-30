3dd470da25e7af9ca52e5e75ae6c8c29d4f2fac1
murilo-hl <murilohl.ifrs@gmail.com>
2025-10-30 14:29:26 -0300

    adicionando dict para as bases complementares

--- functions/prob_mut.py
+++ functions/prob_mut.py
@@ -1,5 +1,7 @@
 probabilities_mut = []
 
+complement = {"A": "T", "T": "A", "C": "G", "G": "C"}]
+
 for i in range(1, len(seq)-1):
     trinuc = seq[i-1] + seq[i] + seq[i+1]
     base_central = seq[i]
@@ -11,10 +13,18 @@ for i in range(1, len(seq)-1):
     
     for b in range(bases):
         chave = f"{trinuc}>{b}"
-        prob = dicionario_sbs4.get(chave, 0)
         
-        if i in hotspots and chave.split(">")[1] in hotspots[i]:
-            prob *= hotspots[i][chave.split(">")[1]]
+        if chave in dicionario_sbs4:
+            prob = dicionario_sbs4[chave]
+        else:
+            trinuc_comp = ( complement[trinuc[0]] + complement[trinuc[1]] + complement[trinuc[2]] )
+            b_comp = complement[b]
+            chave_comp = f"{trinuc_comp}{b_comp}"
+
+            prob = dicionario_sbs4.get(chave_comp, 0)
+        
+        if i in hotspots and b in hotspots[b]
+            prob *= hotspots[i][b]
     
         mut_pos[b] = prob
 
