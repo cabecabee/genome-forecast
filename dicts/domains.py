4613940418ea3c42ecf228dafec106c9efb6cae6
cabecaabe <bernardomb004@gmail.com>
2025-11-12 11:27:42 -0300

    dict de dominios adicionado e função vazia para trabalho futuro

---
+++ dicts/domains.py
@@ -0,0 +1,8 @@
+domains = {
+    "tad": {"start": 1, "end": 63},
+    "prd": {"start": 64, "end": 92},
+    "dbd": {"start": 93, "end": 292},
+    "nls": {"start": 293, "end": 323},
+    "od": {"start": 324, "end": 355},
+    "ctd": {"start": 356, "end": 393}
+}
