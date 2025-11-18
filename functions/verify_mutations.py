6ecdd6d5d02b4b5ad75f5f0f018f07ba7b81cfab
cabecabee <bernardomb004@gmail.com>
2025-11-18 17:55:37 -0300

    verify_mutations agora funciona com um dict de contagem de mutações em cada dominio

--- functions/verify_mutations.py
+++ functions/verify_mutations.py
@@ -1,9 +1,21 @@
 from dicts.domains import domains
+
 def verify_mutations(mutations):
-    # como o analyze mutations já verifica se é nonsense ou missense, podemos simplesmente ver onde ficam as mutações e dar a porcentagem em cada domínio
-    return [(pos, mut_type, get_domain(pos)) for pos, mut_type in mutations]
+    # aqui a função vai retornar um dict contendo o dominio e a quantidade de mutações missense e nonsense nele
+    counts = {
+        domain: {"missense": 0, "nonsense": 0}
+        for domain in domains.keys()
+    }
+
+    for pos, mut_type in mutations:
+        domain = get_domain(pos)
+        if domain is not None:
+            counts[domain][mut_type] += 1
+
+    return counts
 
 def get_domain(pos): # decidi por colocar a função aqui pois só é usada para realizar o verify_mutations, e não quero poluir a pasta functions mais do que já está
         for name, coords in domains.items():
             if coords["start"] <= pos + 1 <= coords["end"]:
                 return name
+        return None
