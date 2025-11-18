dd0bc0ad54f86f1e01a03076b562cc61939ca55e
cabecabee <bernardomb004@gmail.com>
2025-11-18 18:37:11 -0300

    função para a repetição e adição dos valores criada

---
+++ functions/repeat_mutations.py
@@ -0,0 +1,23 @@
+from functions.prob_mut import prob_mut
+from functions.mutate import mutate
+from functions.verify_mutations import verify_mutations
+from functions.analyze_mutations import analyze_mutations
+from dicts.domains import domains
+
+# ☹️
+
+def repeat_mutations(lmbda, seq, p_cumulative, probabilities_mut, loop_amount=10000):
+
+    accumulator_table = {
+        domain: {"missense": 0, "nonsense": 0} for domain in domains.keys()
+    }
+
+    for i in range(loop_amount):
+        mut_seq, mutations = mutate(lmbda, seq, p_cumulative, probabilities_mut)
+        result = analyze_mutations(seq, mut_seq, mutations)
+        current_domain = verify_mutations(result["mut_types"])
+        for domain in domains.keys():
+            accumulator_table[domain]["missense"] += current_domain[domain]["missense"]
+            accumulator_table[domain]["nonsense"] += current_domain[domain]["nonsense"]
+    
+    return accumulator_table
