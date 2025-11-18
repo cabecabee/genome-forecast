2613aa3f2e7a2bbbf3189420fdeea0bfd5a67695
cabecabee <bernardomb004@gmail.com>
2025-11-17 23:00:05 -0300

    análise e verificação dos domínios das mutações quase completa

--- functions/analyze_mutations.py
+++ functions/analyze_mutations.py
@@ -2,34 +2,34 @@ from functions.transcript import transcript
 from functions.translate_linear import translate_linear
 
 def analyze_mutations(orig_seq, mut_seq, mutations):
+    # Transcrição e tradução
     orig_rna = transcript(orig_seq)
     mut_rna = transcript(mut_seq)
     orig_amino = translate_linear(orig_rna)
     mut_amino = translate_linear(mut_rna)
 
-    # achatando as listas de proteínas em uma só sequência linear de aminoácidos
+    # Achatando as listas de proteínas em uma só sequência linear de aminoácidos
     orig_amino = [aa for protein in orig_amino for aa in protein]
     mut_amino = [aa for protein in mut_amino for aa in protein]
 
     diffs = []
+    mut_types = []
+
     for i, (a, b) in enumerate(zip(orig_amino, mut_amino)):
         if a != b:
+            # Determina o tipo individual da mutação
+            if b == "stop":
+                mut_type = "nonsense"
+            else:
+                mut_type = "missense"
             diffs.append((i, a, b))
-    
-    if not diffs:
-        type_mut = "sinônima"
-    else:
-        mut_bases = [b for (_, _, b) in diffs]
-        if "stop" in mut_bases:
-            type_mut = "nonsense"
-        else:
-            type_mut = "missense"
+            mut_types.append((i, mut_type))
 
     changes = {
         "mut_amount": len(mutations),
         "mutations": mutations,
-        "amino_diffs": diffs,
-        "type": type_mut
+        "amino_diffs": diffs,           
+        "mut_types": mut_types
     }
 
     return changes
--- functions/verify_mutations.py
+++ functions/verify_mutations.py
@@ -1,2 +1,9 @@
+from dicts.domains import domains
+def verify_mutations(mutations):
+    # como o analyze mutations já verifica se é nonsense ou missense, podemos simplesmente ver onde ficam as mutações e dar a porcentagem em cada domínio
+    return [(pos, mut_type, get_domain(pos)) for pos, mut_type in mutations]
 
-def verify_mutations():
+def get_domain(pos): # decidi por colocar a função aqui pois só é usada para realizar o verify_mutations, e não quero poluir a pasta functions mais do que já está
+        for name, coords in domains.items():
+            if coords["start"] <= pos + 1 <= coords["end"]:
+                return name
